b731596fd6d004779065de4bf78060a13c936144
shesen8911 <80384403+shesen8911@users.noreply.github.com>
2025-10-03 16:27:35 +0800

    Update NSBundle+YTLite.m

--- Utils/NSBundle+YTLite.m
+++ Utils/NSBundle+YTLite.m
@@ -8,11 +8,11 @@
 
     dispatch_once(&onceToken, ^{
         NSString *tweakBundlePath = [[NSBundle mainBundle] pathForResource:@"YTLite" ofType:@"bundle"];
-        #if ROOTLESS
-            NSString *bundlePath = ROOT_PATH_NS("/Library/Application Support/YTLite.bundle");
-        #else
+        #if defined(ROOTHIDE) && __has_include(<roothide.h>)
             #import <roothide.h>
             NSString *bundlePath = jbroot(@"/Library/Application Support/YTLite.bundle");
+        #else
+            NSString *bundlePath = ROOT_PATH_NS("/Library/Application Support/YTLite.bundle");
         #endif
 
         bundle = [NSBundle bundleWithPath:tweakBundlePath ?: bundlePath];
