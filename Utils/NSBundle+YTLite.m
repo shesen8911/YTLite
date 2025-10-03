f0de65426e835eb0642ddfaa998f3aa21f1914b2
shesen8911 <80384403+shesen8911@users.noreply.github.com>
2025-10-03 15:50:42 +0800

    Typo

--- Utils/NSBundle+YTLite.m
+++ Utils/NSBundle+YTLite.m
@@ -7,6 +7,7 @@
     static dispatch_once_t onceToken;
 
     dispatch_once(&onceToken, ^{
+        NSString *tweakBundlePath = [[NSBundle mainBundle] pathForResource:@"YTLite" ofType:@"bundle"];
         #if ROOTLESS
         NSString *bundlePath = ROOT_PATH_NS("/Library/Application Support/YTLite.bundle");
         #else
